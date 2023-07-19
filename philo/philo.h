922495af82e34f7552edbcbbace5193260b75208
Markus Laaksonen <markus@mxa.fi>
2023-07-19 08:01:38 +0300

    add necessary includes

--- philo/philo.h
+++ philo/philo.h
@@ -6,13 +6,29 @@
 /*   By: malaakso <malaakso@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 22:05:51 by malaakso          #+#    #+#             */
-/*   Updated: 2023/05/17 22:13:01 by malaakso         ###   ########.fr       */
+/*   Updated: 2023/07/19 08:01:23 by malaakso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
+// for memset
+# include <string.h>
 
+// for printf
+# include <stdio.h>
+
+// for malloc, free
+# include <stdlib.h>
+
+// for write, usleep
+# include <unistd.h>
+
+// for gettimeofday
+# include <sys/time.h>
+
+// for pthread functions
+# include <pthread.h>
 
 #endif
