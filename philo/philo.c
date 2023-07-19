66626aaf70e037cd0e1e34b57ae055564a1ff1ca
Markus Laaksonen <markus@mxa.fi>
2023-07-19 08:02:46 +0300

    add todo remove extra cflags from Makefile

--- philo/philo.c
+++ philo/philo.c
@@ -6,12 +6,13 @@
 /*   By: malaakso <malaakso@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 22:04:32 by malaakso          #+#    #+#             */
-/*   Updated: 2023/05/17 22:19:47 by malaakso         ###   ########.fr       */
+/*   Updated: 2023/07/19 07:55:36 by malaakso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	main(int ac, char **av)
 {
+	// DEBUG: Makefile has debug flags and extra CFLAGS to remove before submission
 	(void)ac;
 	(void)av;
 	return (0);
