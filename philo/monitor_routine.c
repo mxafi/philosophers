9dcd71bfc48fa6f273a03d16f8459ffef8a91b47
Markus Laaksonen <markus@mxa.fi>
2023-07-24 13:49:25 +0300

    started monitor_routine

---
+++ philo/monitor_routine.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   monitor_routine.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malaakso <malaakso@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/24 13:15:32 by malaakso          #+#    #+#             */
+/*   Updated: 2023/07/24 13:48:59 by malaakso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+t_bool	is_philosopher_fed(int *n_of_fed_philosophers, t_common_data *d,
+	t_philosopher *p)
+{
+	t_bool	ret;
+
+	if (d->number_of_times_each_philosopher_must_eat == -1)
+		return (FALSE);
+	ret = FALSE;
+	pthread_mutex_lock(&p->eat_finished_lock);
+	if (p->eat_finished == TRUE)
+	{
+		*n_of_fed_philosophers++;
+		ret = TRUE;
+	}
+	pthread_mutex_unlock(&p->eat_finished_lock);
+	return (ret);
+}
+
+t_bool	is_philosopher_dead(t_philosopher *p)
+{
+	//todo
+}
+
+void	*monitor_routine(t_common_data *d)
+{
+	int	i;
+	int	n_of_fed_philosophers;
+
+	while (1)
+	{
+		usleep(500);
+		i = 0;
+		n_of_fed_philosophers = 0;
+		while (i < d->number_of_philosophers
+			&& n_of_fed_philosophers != d->number_of_philosophers)
+		{
+			if (is_philosopher_fed(&n_of_fed_philosophers,
+					d, d->philosophers[i]) == FALSE)
+			{
+				if (is_philosopher_dead(d->philosophers[i]) == TRUE)
+					return (NULL);
+			}
+			i++;
+		}
+		if (n_of_fed_philosophers == d->number_of_philosophers)
+			return (NULL);
+	}
+	return (NULL);
+}
