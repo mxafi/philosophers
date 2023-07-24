77808e7ae660862944f2421934a5e0364a645381
Markus Laaksonen <markus@mxa.fi>
2023-07-24 10:06:41 +0300

    add philo_routine

---
+++ philo/philo_routine.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_routine.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malaakso <malaakso@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/24 09:53:14 by malaakso          #+#    #+#             */
+/*   Updated: 2023/07/24 10:06:18 by malaakso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	*philosopher_routine(t_philosopher *p)
+{
+	if (p->philo_id % 2 == 0)
+	{
+		//sleep first
+	}
+	while (1)
+	{
+		//eat first
+		//sleep
+		//think: try grabbing forks
+	}
+	return (NULL);
+}
